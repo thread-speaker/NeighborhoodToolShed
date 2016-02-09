8deac17bb372ca387d8e4d19b27bad5b45cb3aef
Curtis Merrell <Curtis Merrell>
2016-02-08 17:15:16 -0700

    Added a profile page and controller. Tested some filtering functionality on the Home Controller to filter user results.

--- web/controllers/homeCtrl.js
+++ web/controllers/homeCtrl.js
@@ -1,5 +1,35 @@
 var app = angular.module('neighborToolApp');
 
 app.controller('homeViewCtrl', function($scope) {
-	$scope.hello = "hi"
+	$scope.commonTools = ["hammer", "saw", "wrench"];	
+	
+	$scope.usersNearby = //give me all the users within (x) miles
+	[
+		{
+			"username" : "csmerrell",
+			"tools" : ["hammer", "saw", "screwdriver"]
+		},
+		{
+			"username" : "somedude",
+			"tools" : ["screwdriver", "level", "wrench"]
+		}
+	];
+	
+	$scope.usersWithTool = function(str) {
+		if(!str)
+			return $scope.usersNearby;
+
+		var filteredUsers = [];
+		for(i = 0; i < $scope.usersNearby.length; i++){
+			var tools = $scope.usersNearby[i].tools;
+			for(j = 0; j < tools.length; j++){
+				if(tools[j].indexOf(str) != -1) {
+					filteredUsers.push($scope.usersNearby[i])
+					break;
+				}
+			}
+		}
+		return filteredUsers;
+	}
+	
 });
