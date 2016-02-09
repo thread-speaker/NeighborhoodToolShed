10e9d88d1b90ddbf4fe40a0bdc767c8434b213c2
Curtis Merrell <Curtis Merrell>
2016-02-09 01:58:19 -0700

    Renamed home page files to 'dashboard'. Implemented full search inputs on the default dashboard page.

--- web/controllers/dashboardCtrl.js
+++ web/controllers/dashboardCtrl.js
@@ -1,7 +1,38 @@
 var app = angular.module('neighborToolApp');
 
-app.controller('homeViewCtrl', function($scope) {
+app.controller('dashboardCtrl', function($scope) {
+	$scope.commonQueries = [
+		{
+			"query" : ""
+		}
+	];
+	
+	$scope.addDropDown = function(input) {
+		var idx = $scope.commonQueries.indexOf(input) + 1;
+		$scope.commonQueries.splice(idx, 0,
+			{ "query" : "" }
+		);
+	};
+
+	$scope.customQueries = [
+		{
+			"query" : ""
+		}
+	];
+
+	$scope.addSearch = function(input) {
+		var idx = $scope.customQueries.indexOf(input) + 1;
+		$scope.customQueries.splice(idx, 0,
+			{ "query" : "" }
+		);
+	};
+
 	$scope.commonTools = ["hammer", "saw", "wrench"];	
+	
+	$scope.goClick = function() {
+		var toolFilters = $scope.commonQueries.concat($scope.customQueries);
+	}
+	
 	
 	$scope.usersNearby = //give me all the users within (x) miles
 	[
