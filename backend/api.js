4f88bbfb60496e586c0795f4ed67add9050cd69d
Aaron Monson <aaron@lmonson.com>
2016-02-17 08:01:23 -0700

    Cleaned up the dependancies and skeleton api.

--- backend/api.js
+++ backend/api.js
@@ -12,7 +12,26 @@ app.use(bodyParser.urlencoded({
 //
 
 // example GET api endpoint
-app.get('/api', function (req, res) {
-  console.log("api was accessed!");
+app.get('/api/path/to/endpoint', function (req, res) {
+	console.log("api was accessed!");
 	res.json({api: true});
+});
+
+app.post('/api/users/register', function (req, res) {
+	res.json({created: false, message:"Register not implemented"});
+});
+
+app.post('/api/users/login', function (req, res) {
+	res.json({created: false, message:"Login not implemented"});
+});
+
+app.get('/api/users/profile', function (req, res) {
+	res.json({profile: null, message:"Get profile not implemented"});
+});
+
+//Update a profile by id
+app.put('/api/users/profile/:uid', function (req, res) {
+	//uid is a var holding the profile id
+	var profile_id = uid;
+	res.json({updated: false, message:"Profile update not implemented"});
 });
