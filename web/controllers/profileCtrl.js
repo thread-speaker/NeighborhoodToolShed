7eabcedf3b8d6aff1176fba8a34bb4ef916e8c81
Paul Cloward <paulclowardcs12@gmail.com>
2016-02-22 22:14:56 -0700

    ng-repeat user tool list

--- web/controllers/profileCtrl.js
+++ web/controllers/profileCtrl.js
@@ -2,4 +2,12 @@ var app = angular.module('neighborToolApp');
 
 app.controller('profileCtrl', function($scope) {
 	$scope.data = [];
+	
+	var ownedTools = [
+		
+		{tool: "hammer", status: "out"},
+		{tool: "screwdriver", status: "in"} 
+		
+	]
+	$scope.ownedTools = ownedTools;
 });
