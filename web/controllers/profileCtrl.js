58ab5b13dbc16cac0fd5f506d11ff792562ee477
Paul Cloward <paulclowardcs12@gmail.com>
2016-02-25 15:20:46 -0700

    add user tool to list feature

--- web/controllers/profileCtrl.js
+++ web/controllers/profileCtrl.js
@@ -1,13 +1,38 @@
 var app = angular.module('neighborToolApp');
 
 app.controller('profileCtrl', function($scope) {
-	$scope.data = [];
+	
+	$scope.userData = 
+		{ name: "Paul Cloward", profileImg: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAKvAAAAJDRkZjNhYmE5LTIzNWUtNGU0ZS04ZTBmLWVhMmYwZjY2NThmYg.jpg",
+			address: "Provo, Utah", preferredContactInfo: "Provo Forever"}
 	
 	var ownedTools = [
 		
 		{tool: "hammer", status: "out"},
-		{tool: "screwdriver", status: "in"} 
+		{tool: "screw driver", status: "in"},
+		{tool: "screw driver", status: "in"}, 
+		{tool: "screw driver", status: "in"},
+		{tool: "screw driver", status: "in"}, 
 		
 	]
 	$scope.ownedTools = ownedTools;
+	
+	
+	
+	$scope.addTool = function() {
+		
+		var x = document.getElementById("myText").value;
+		
+		if(x != "")
+		$scope.ownedTools.push({tool: x, status: "in"});
+	}
+	
+	var rentalTools = [
+	
+		{tool: "screw driver", dueDate: "02/2/16"},
+		{tool: "wrench", dueDate: "12/8/16"},
+		{tool: "screws", dueDate: "04/1/16"},
+		{tool: "sledge hammer", dueDate: "03/2/16"}
+	]
+	$scope.rentalTools = rentalTools;
 });
