052e60108b979a2a978a8053cadbaed0bf511376
Paul Cloward <paulclowardcs12@gmail.com>
2016-02-26 15:49:44 -0700

    changes to the add User Tools feature. Not finished

--- web/controllers/profileCtrl.js
+++ web/controllers/profileCtrl.js
@@ -17,14 +17,44 @@ app.controller('profileCtrl', function($scope) {
 	]
 	$scope.ownedTools = ownedTools;
 	
-	
+	$scope.allProgramTools = [
+		
+		{parentTool: "hammer", childrenTools: ["war", "sledge"], clicked: false},
+		{parentTool: "screw driver", childrenTools: ["philips", "flat"], clicked: false},
+	];
 	
 	$scope.addTool = function() {
 		
-		var x = document.getElementById("myText").value;
+		$scope.displayProgramTools = true;
 		
-		if(x != "")
-		$scope.ownedTools.push({tool: x, status: "in"});
+		$scope.doneButtonDisplay = true;
+	}
+	
+	/*endAddTool- Function sets the clicked variable in each object in the allProgramTools array to false.
+					Also closes the displayProgramsTools section. This function is used when the
+					"Done" button is clicked.*/
+	$scope.endAddTool = function() {
+		
+		$scope.displayProgramTools = false;
+		
+		for(var i = 0; i < $scope.allProgramTools.length; i++) {
+			
+			$scope.allProgramTools[i].clicked = false;
+		}
+		
+		$scope.doneButtonDisplay = false;
+	}
+	
+	$scope.displayProgramTools = false;
+	
+	$scope.doneButtonDisplay = false;
+	
+	$scope.displayChildrenTools = function(parentTool){
+		
+		if(parentTool.clicked === false)
+			parentTool.clicked = true;
+		else
+			parentTool.clicked = false;
 	}
 	
 	var rentalTools = [
