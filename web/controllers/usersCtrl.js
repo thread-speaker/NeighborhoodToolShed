7c8f1273e2a7f949af27a7b228166c24dbe5499a
Curtis Merrell <Curtis Merrell>
2016-02-27 11:59:56 -0700

    Got the searchCriteria functionality set up. Sends selected tool species to the users page as a window.localStorage JSON

--- web/controllers/usersCtrl.js
+++ web/controllers/usersCtrl.js
@@ -1,32 +1,29 @@
 var app = angular.module('neighborToolApp');
 
 app.controller('usersCtrl',
-  function($scope, $route, $firebaseArray) {
-    var ref = new Firebase("https://amber-torch-1283.firebaseio.com/");
-		var tools = ref.child("tools");
-
-    //Grab search queries
-    $scope.queries = $route.current.params.filter.split(',');
-
-    //Add them to the ng-repeat array;
-    $scope.queryResults = $firebaseArray(tools.orderByChild("genus"));
-
-    $scope.matchesQueries = function(value, index, array){
-        return $scope.queries.indexOf(value.genus) >= 0;
-    };
-
-    // for (var i in $scope.queries){
-    //   var query = $scope.queries[i];
-    //   tools.orderByChild("genus")
-    //       .startAt(query).endAt(query + "\uf8ff")
-    //       .on("child_added", function(snapshot){
-    //         $scope.$apply(function(){  //Do it NOW
-    //           $scope.queryResults.push(snapshot.val());
-    //         });
-    //       });
-    // }
-
-
-
-  }
+	function($scope, $route, $firebaseArray) {
+		var toolsSearched = [];
+		if(window.localStorage.toolsSearched)
+			toolsSearched = JSON.parse(window.localStorage.toolsSearched)
+		
+		if(toolsSearched.length > 0)
+			$scope.usersToDisplay = usersWithTools(toolsSearched);
+		else
+			$scope.usersToDisplay = [
+				{
+					"username" : "EVERYONE"
+				}
+			]
+	}
 );
+
+function usersWithTools(toolsSearched) {
+	return [
+		{
+			"username" : "Curt"
+		},
+		{
+			"username" : "Paul"
+		}
+	];
+}
