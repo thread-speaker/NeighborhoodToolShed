2354719f1d4a2e00f96959a00210f33e2370b94b
Paul Cloward <paulclowardcs12@gmail.com>
2016-02-29 21:42:03 -0700

    checkbox feature completed for add tool section of userprofile

--- web/controllers/profileCtrl.js
+++ web/controllers/profileCtrl.js
@@ -3,27 +3,20 @@ var app = angular.module('neighborToolApp');
 app.controller('profileCtrl', function($scope) {
 	
 	$scope.userData = 
-		{ name: "Paul Cloward", profileImg: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAKvAAAAJDRkZjNhYmE5LTIzNWUtNGU0ZS04ZTBmLWVhMmYwZjY2NThmYg.jpg",
+		{ name: "Cole Reasch", profileImg: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAKvAAAAJDRkZjNhYmE5LTIzNWUtNGU0ZS04ZTBmLWVhMmYwZjY2NThmYg.jpg",
 			address: "Provo, Utah", preferredContactInfo: "Provo Forever"}
 	
-	var ownedTools = [
-		
-		{tool: "hammer", status: "out"},
-		{tool: "screw driver", status: "in"},
-		{tool: "screw driver", status: "in"}, 
-		{tool: "screw driver", status: "in"},
-		{tool: "screw driver", status: "in"}, 
-		
-	]
-	$scope.ownedTools = ownedTools;
+
+	$scope.ownedTools = [];
+	$scope.testTools = [];
 	
 	$scope.allProgramTools = [
 		
-		{parentTool: "hammer", childrenTools: ["war", "sledge"], clicked: false},
-		{parentTool: "screw driver", childrenTools: ["philips", "flat"], clicked: false},
+		{genusTool: "hammer", speciesTools: ["war", "sledge"], clicked: false},
+		{genusTool: "screw driver", speciesTools: ["phillips", "flat"], clicked: false},
 	];
 	
-	$scope.addTool = function() {
+	$scope.addToolButton = function() {
 		
 		$scope.displayProgramTools = true;
 		
@@ -49,12 +42,81 @@ app.controller('profileCtrl', function($scope) {
 	
 	$scope.doneButtonDisplay = false;
 	
-	$scope.displayChildrenTools = function(parentTool){
+	$scope.displayChildrenTools = function(genusTool){
 		
-		if(parentTool.clicked === false)
-			parentTool.clicked = true;
+		if(genusTool.clicked === false)
+			genusTool.clicked = true;
 		else
-			parentTool.clicked = false;
+			genusTool.clicked = false;
+	}
+	
+	$scope.testA = [];
+
+	$scope.isGenusToolOwned = function(gTool) {
+		
+		for(var i = 0; i < $scope.ownedTools.length; i++) {
+			
+			if(gTool.genusTool === $scope.ownedTools[i].genusTool) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	$scope.isToolOwned = function(gTool, sTool){
+		for(var i = 0; i < $scope.ownedTools.length; i++) {
+			
+			if(gTool.genusTool === $scope.ownedTools[i].genusTool) {
+				
+				var speciesTools = $scope.ownedTools[i].speciesTools;
+				for(var x = 0; x < speciesTools.length; x++) {
+					
+					if(sTool === speciesTools[x]) {
+						return x;
+					}
+				}
+			}
+		}
+		return -1;
+	}
+	
+	$scope.isToolOwnedBoolean = function(gTool, sTool){
+		for(var i = 0; i < $scope.ownedTools.length; i++) {
+			
+			if(gTool.genusTool === $scope.ownedTools[i].genusTool) {
+				
+				var speciesTools = $scope.ownedTools[i].speciesTools;
+				for(var x = 0; x < speciesTools.length; x++) {
+					
+					if(sTool === speciesTools[x]) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	$scope.addTool = function(gTool, sTool){
+		
+		var speciesIndex = $scope.isToolOwned(gTool,sTool);
+		
+		var genusIndex = $scope.isGenusToolOwned(gTool);
+		
+		if(genusIndex === -1) {
+			$scope.ownedTools.push({genusTool: gTool.genusTool, speciesTools: [sTool]});
+		} else if(speciesIndex === -1 && genusIndex != -1) {
+			$scope.ownedTools[genusIndex].speciesTools.push(sTool);
+		} else {
+	
+			if($scope.ownedTools[genusIndex].speciesTools.length === 1) {
+				
+				$scope.ownedTools.splice(genusIndex, 1);
+			}
+			else {
+				$scope.ownedTools[genusIndex].speciesTools.splice(speciesIndex, 1);
+			}
+		}
 	}
 	
 	var rentalTools = [
