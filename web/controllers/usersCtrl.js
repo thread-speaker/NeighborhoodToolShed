a81a4247d9fe7841bee7337dff7f11595af46587
Clint <wolf123450@gmail.com>
2016-03-10 13:44:44 -0700

    finished making a sorted (by distance) unique array of users who have a certain tool

--- web/controllers/usersCtrl.js
+++ web/controllers/usersCtrl.js
@@ -1,8 +1,9 @@
 var app = angular.module('neighborToolApp');
 
 app.controller('usersCtrl',
-	function($scope, $route, $firebaseObject) {
+	function($scope, $route, $firebaseObject, $filter) {
 		//Get Location
+		var orderBy = $filter('orderBy');
 		var location = {};
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
@@ -22,25 +23,50 @@ app.controller('usersCtrl',
 		if(window.localStorage.toolsSearched)
 			toolsSearched = JSON.parse(window.localStorage.toolsSearched)
 
-    var users = {};
+    var users = [];
+		// var users = {};
     for (var t in toolsSearched){
       var tool = toolsSearched[t];
       var toolsUsers = newTools.child(tool.genus).child(tool.species).child("users").on("child_added", function(snap){
         var uid = snap.key();
-        console.log(uid);
-        var user = $scope.users.child(uid).on("child_added", function(snap){
-					console.log("apply");
-          var key = snap.key();
-          var val = snap.val();
+        // console.log(uid);
+        var user = $firebaseObject($scope.users.child(uid));
+				user.$loaded(function(){
+					var distance = $scope.distance(location, user.location);
+					user.distance = distance;
+					users.sort(function(a,b){  //Sort by distance from here;
+						return a.distance - b.distance;
+					});
+					$scope.usersToDisplay = users.filter(function(item, pos, arry) {  //Get rid of duplicate uids
+        		return !pos || item["$id"] != arry[pos - 1]["$id"];
+    			});
+				});
+				users.push(user);
+				// user.on("child_added", function(snap){
+				// 	// console.log("apply");
+        //   var key = snap.key();
+        //   var val = snap.val();
+				//
+				//
+        //   if (!users.hasOwnProperty(uid)){
+        //     users[uid] = {};
+				// 	}
+				// 	if (key == "location"){
+				// 		var distance = $scope.distance(location, val);
+				// 		users[uid]["distance"] = distance;
+				// 	}
+				//
+        //   $scope.$apply(function(){users[uid][key] = val;});
+				//
+        //   // console.log("users" + users);
+        // });
 
-          if (!users.hasOwnProperty(uid)){
-            users[uid] = {};
-					}
 
-          $scope.$apply(function(){users[uid][key] = val;});
-
-          console.log("users" + users);
-        });
+				// user.once("value", function(snap){
+				// 	console.log("test");
+				// 	users = orderBy(users, "distance", false);
+				// });
+				//
       });
     }
 
@@ -55,11 +81,30 @@ app.controller('usersCtrl',
 
     function usersWithTools(toolsSearched) {
         // console.log(toolsSearched);
-        first = toolsSearched[0];
-        test = $firebaseObject(newTools.child(first.genus).child(first.species));
+        // first = toolsSearched[0];
+        // test = $firebaseObject(newTools.child(first.genus).child(first.species));
         // test.$loaded(function(){});
         return users;
-      }
+      };
+
+			$scope.testOrder = function(predicate){
+				console.log("predicate");
+				return 0;
+			}
+
+			$scope.distance = function (loc1, loc2) { //miles
+			  lat1 = loc1.latitude;
+			  lon1 = loc1.longitude;
+			  lat2 = loc2.latitude;
+			  lon2 = loc2.longitude;
+			  var p = 0.017453292519943295;    // Math.PI / 180
+			  var c = Math.cos;
+			  var a = 0.5 - c((lat2 - lat1) * p)/2 +
+			          c(lat1 * p) * c(lat2 * p) *
+			          (1 - c((lon2 - lon1) * p))/2;
+
+			  return 12742 * Math.asin(Math.sqrt(a)) * 0.621371; // 2 * R; R = 6371 km
+			};
 	}
 );
 
@@ -72,17 +117,3 @@ app.controller('usersCtrl',
 // 		"username" : "Paul"
 // 	}
 // ];
-
-function distance(loc1, loc2) { //miles
-  lat1 = loc1.latitude;
-  lon1 = loc1.longitude;
-  lat2 = loc2.latitude;
-  lon2 = loc2.longitude;
-  var p = 0.017453292519943295;    // Math.PI / 180
-  var c = Math.cos;
-  var a = 0.5 - c((lat2 - lat1) * p)/2 +
-          c(lat1 * p) * c(lat2 * p) *
-          (1 - c((lon2 - lon1) * p))/2;
-
-  return 12742 * Math.asin(Math.sqrt(a)) * 0.621371; // 2 * R; R = 6371 km
-}
